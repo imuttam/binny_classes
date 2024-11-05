bfdc83d4ad684c22dc2d3ad35cb40397c44cd55e
imuttam <uttam.uk@gmail.com>
2024-11-05 21:17:36 +0530

    grade_check 051124

---
+++ decision/grade_check.py
@@ -0,0 +1,15 @@
+# marks > 80--A, marks>70--B, marks>60--C,  else D
+
+marks = int(input("Enter a your marks: "))
+
+if marks > 80:
+    print("Your grade is 'A'")
+
+elif marks > 70:
+    print("Your grade is 'B'")
+
+elif marks > 60:
+    print("Your grade is 'C'")
+
+else:
+    print("Your grade is 'D'")
