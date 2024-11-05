b79ef30291dfe9fdeb258bc0e1982ae7cdcb1ecc
imuttam <uttam.uk@gmail.com>
2024-11-05 21:07:45 +0530

    first commit

---
+++ basic.py
@@ -0,0 +1,3 @@
+# int, float
+# operators: +, - , *, /,   %(modulo/remainder), //(floor)
+# condition:(decision)---if ,elif, else
---
+++ odd_even.py
@@ -0,0 +1,11 @@
+# To check weather a number is odd or even
+# x = 111
+# to take input from user
+x = input("Enter a number: ")# input is always a text/string
+x = int(x)
+
+if x%2 == 0:
+    print("Even")
+else:
+    print("Odd")
+                    
